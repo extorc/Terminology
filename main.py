cde22ec17dbbd470d1fac9742a9b892c3e8d9ae5
Extorc Studios <mittalshaurya004@gmail.com>
2021-02-26 12:10:31 +0000

    full functionality

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ db["terminology"] = {}
 
 @bot.command()
 async def add(ctx, *,added_termy):
-	
 	valid = False
 	for s in added_termy:
 		if s == ":":
@@ -17,7 +16,7 @@ async def add(ctx, *,added_termy):
 			valid = False
 	if valid:
 		termy_buffer = added_termy.split(":")
-		print(termy_buffer)
+		termy_buffer[0] = termy_buffer[0].strip()
 		termy[termy_buffer[0]] = termy_buffer[1]
 		db["terminology"] = termy
 		await ctx.send("Added the Terminology : "+ added_termy[:10]+ "....")
@@ -27,7 +26,12 @@ async def add(ctx, *,added_termy):
 
 @bot.command()
 async def get(ctx, query):
-	await ctx.send(db["terminology"][query])
+	await ctx.send("```" + db["terminology"][query] + "```")
+	
+@bot.command()
+async def get_terms(ctx):
+	for k in list(db["terminology"].keys()):
+		await ctx.send("```"+k+"```")
 
 @bot.event
 async def on_ready():
